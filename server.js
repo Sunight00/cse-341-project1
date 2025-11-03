21b603be7c0c06ef32b361dab2acbf7322f734d3
Sunight00 <ephraim.ewane@gmail.com>
2025-11-03 20:26:12 +0100

    great

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const port = process.env.PORT || 3000;
+
+const mongodb = require("./data/database")
+
+app.use('/', routes)
+
+
+mongodb.initDb((err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        app.listen(port, () => { console.log(`Database is running on port ${port}`);});
+    }
+});
