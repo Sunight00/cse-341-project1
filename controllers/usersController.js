66ef2914e369b945654767e5fe0fd921c6ecf889
Sunight00 <ephraim.ewane@gmail.com>
2025-11-03 21:33:38 +0100

    great

---
+++ controllers/usersController.js
@@ -0,0 +1,22 @@
+const mongodb = require("../data/database");
+const ObjectId = require("mongodb").ObjectId;
+const usersController = {};
+
+usersController.getAll = async (req, res) => {
+    const result = await mongodb.getDatabase().db("users").collection("users").find()//.toArray();
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users);
+    })
+};
+
+usersController.getSingle = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db("users").collection("users").find({_id: userId})//.toArray();
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users[0]);
+    })
+};
+
+module.exports = usersController;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ router.get('/', (req, res) =>{
     res.send('Hello, World!')
 })
 
+router.use("/users", require("./users"))
 
 
 
